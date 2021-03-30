e01510d2205e1290f165a3b05cdea8914e70204c
kenn <kenn.balendres@gmail.com>
2021-03-30 09:39:58 +0800

    updated

--- resources/views/files/edit.blade.php
+++ resources/views/files/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Product</h2>
+                <h2>Edit File</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('files.index') }}"> Back</a>
--- resources/views/files/view.blade.php
+++ resources/views/files/view.blade.php
@@ -1,7 +1,23 @@
 @extends('files.layout')
 @section('content')
-    <h2>{{$file->FileName}}</h2>
-    <h2>{{ asset('storage/'.$file->path) }}</h2>
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>View File</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('files.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>File Name:</strong></br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            {{ $file->FileName }}
+        </div>
+    </div>
 
     <div class="col-md-6-offset-4">
                         {{-- <embed src="{{ asset('storage/'.$file->path) }}" type="$file->FileType" width="100%" height="600px" />--}}
